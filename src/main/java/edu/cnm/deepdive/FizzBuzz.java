2567436c9e80424a38f9947b90c02353ce26bbc5
dagoberto <berto35572@gmail.com>
2021-06-10 09:18:57 -0600

    defined String and a switch

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -2,4 +2,30 @@ package edu.cnm.deepdive;
 
 public class FizzBuzz {
 
-}
+  public static String fizzbuzz (int value) {
+    String result;
+      switch (value % 15){
+        case 0:
+          result = "FizzBuzz";
+          break;
+        case 3:
+        case 6:
+        case 9:
+        case 12:
+          result = "Fizz";
+          break;
+        case 5:
+        case 10:
+          result = "Buzz";
+          break;
+        default:
+          result = String.valueOf(value);
+          break;
+
+
+      }
+
+    return result;
+  }
+
+    }
